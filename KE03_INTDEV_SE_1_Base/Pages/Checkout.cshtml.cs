95d68ad7b1c2964e1453c540674b84616f23bab1
camiel schnackers <camielschnackers1@gmail.com>
2025-06-08 23:30:00 +0200

    checkout pagina aangemakt door gebruik oude structuur winkelwagen.
    - productcatalog aangepast om producten te koppelen naar de checkout pagina.

--- KE03_INTDEV_SE_1_Base/Pages/Checkout.cshtml.cs
+++ KE03_INTDEV_SE_1_Base/Pages/Checkout.cshtml.cs
@@ -1,12 +1,35 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using DataAccessLayer.Models;
+using DataAccessLayer.Interfaces;
 
 namespace KE03_INTDEV_SE_1_Base.Pages
 {
     public class CheckoutModel : PageModel
     {
+        private readonly IProductRepository _productRepository;
+
+        public CheckoutModel(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        [BindProperty(SupportsGet = true)]
+        public int ProductId { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public int Quantity { get; set; } = 1;
+
+        public Product? Product { get; set; }
+
         public void OnGet()
         {
+            Product = _productRepository.GetProductById(ProductId);
+        }
+
+        public void OnPost()
+        {
+            Product = _productRepository.GetProductById(ProductId);
         }
     }
 }
